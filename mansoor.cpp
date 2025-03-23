5d43dfd6549358ee2ec97dfc8f4293591e69cfaa
mansoor <t.alyamani95@gmail.com>
2025-03-23 23:40:15 +0700

    prompting user to enter array elemnts

--- mansoor.cpp
+++ mansoor.cpp
@@ -4,8 +4,23 @@ int arr[20] // membuat Array dengan panjang data 20
 int n; //membuat Variable inpuan n
 void input(){
 //procedure input
-while (true){
-    cout<<"masukkan jumlah data pada array">>;//membuat inputan jumlah elment array
-    cin>>n;
-}
+    while (true){
+      cout<<"masukkan jumlah data pada array">>;//membuat inputan jumlah elment array
+      cin>>n;//memanggil variable inputan n
+      if (n<=20)
+      {
+        //membuat kondisi n tidak lebih dari 20
+        break;
+      }
+      else
+      {
+        cout<<"\n narray yang anda masukan maksimal 20elmen.\n"; //menamilkan pesan jika data lebih dari 20 
+      }
+      
+      cout<<endl; //membuat jarka per baris program
+      cout<<"======================="<<endl;//membuat tampilan susunan data element array
+      cout<<"masukan elment array"<<endl;
+      cout<<"======================="<<endl;
+    }
+
 }
