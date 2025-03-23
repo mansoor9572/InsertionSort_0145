57979df89646bb82209fc713f54c4c2f0c6a516d
mansoor <t.alyamani95@gmail.com>
2025-03-23 23:23:35 +0700

    user inut pvalidation

--- mansoor.cpp
+++ mansoor.cpp
@@ -2,3 +2,10 @@
 using namespace std;
 int arr[20] // membuat Array dengan panjang data 20
 int n; //membuat Variable inpuan n
+void input(){
+//procedure input
+while (true){
+    cout<<"masukkan jumlah data pada array">>;//membuat inputan jumlah elment array
+    cin>>n;
+}
+}
